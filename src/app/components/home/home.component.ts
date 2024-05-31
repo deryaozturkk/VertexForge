d16fa08a69f36ec075d10f46fec5377389de42e7
Derya ÖZTÜRK <19360859009@ogrenci.btu.edu.tr>
2024-05-31 16:56:29 +0300

    home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';  // ButtonModule'ü ekleyin
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardModule, ButtonModule],  // ButtonModule'ü burada ekleyin
+  imports: [CardModule, ButtonModule,CommonModule],  // ButtonModule'ü burada ekleyin
   templateUrl: './home.component.html',
   styleUrls: ["./home.component.scss"]
 })
